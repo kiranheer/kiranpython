dea2a83fe8abd64928edcd1ef9f100cdcafb4dc2
kiranheer <doliheer@gmail.com>
2018-01-25 14:36:48 +0530

    this is a programming language

---
+++ indexingANDmethod.py
@@ -0,0 +1,21 @@
+s='this is a good girl'
+print(s)
+a=s.capitalize()
+print(a)
+
+a1=s.islower()
+print(a1)
+
+#LIST
+list=[1,2,3,4]
+print(list)
+country=['india','usa','uk','pak','canada']
+print(country)
+ac=country.append('aus')
+print(ac)
+w=country[1:4]
+print(w)
+country=['india:1','usa:2','uk:3','pak:4','canada:5']
+
+print(country)
+
---
+++ list.py
@@ -0,0 +1,22 @@
+list=[1,2,3,4,5,6,7,8,9]
+print(list)
+#1....
+p=list[ :8]
+print(p)
+#2....
+p1=list[1:]
+print(p1)
+
+p2=list[1:2:1]
+print(p2)
+
+p3=list[-3]
+print(p3)
+
+a=len(list)
+print(a)
+c=list[1:2:1]
+print(c)
+
+c1=list[1:6:1]
+print(c1)
