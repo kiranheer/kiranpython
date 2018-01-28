8bcf30c2dd160d3d746a6ed2725294a6823ff712
kiranheer <doliheer@gmail.com>
2018-01-28 20:41:32 +0530

    i am learn list in python

--- list.py
+++ list.py
@@ -6,17 +6,31 @@ print(p)
 #2....
 p1=list[1:]
 print(p1)
-
+print("this is slicing the list")
 p2=list[1:2:1]
 print(p2)
-
+print("1........#####")
 p3=list[-3]
 print(p3)
-
+print("2......########")
 a=len(list)
 print(a)
+print("3.....#########")
 c=list[1:2:1]
 print(c)
-
+print("4.....#######")
 c1=list[1:6:1]
 print(c1)
+z=list[-1:-4]
+print(z)
+print("5......######")
+x=list[-1:-2:-4]
+print(x)
+
+ax='i' in 'classroom'
+print(ax)
+
+xx='2' in 'list'
+print(xx)
+n=input("enter the no..")
+print(n)
